7ea6381d5e63a888bf96afa8f046d981783ce194
lizhouwei <lizhouwei518@163.com>
2019-01-21 22:08:24 +0800

    菜单资源优化

--- src/store/modules/menu/index.js
+++ src/store/modules/menu/index.js
@@ -1,14 +1,24 @@
+import axios from 'utils/ajax'
 
 const state = {
     isCollapse: false,
     navList: [],
     topNavList: [],
-    sideNavList: []
+    sideNavList: [],
+    menuTree:[],
+    flatMenuList:[]
+    
 }
 
 const mutations = {
     setcollapse : state => {
         state.isCollapse  = !state.isCollapse
+    },
+    setMenuTree : (state, data) =>{
+        state.menuTree = data
+    },
+    setFlatMenuList:(state, data)=>{
+        state.flatMenuList = data
     },
     setNavList: (state, data) => {
         state.navList = data
@@ -24,6 +34,50 @@ const mutations = {
 const actions = {
     toggleSideBar({ commit }) {
         commit('setcollapse')
+    },
+     // 获取该用户的菜单列表
+     getMenuTree({dispatch,commit},params){
+        return new Promise((resolve) =>{
+           axios({
+               url: '/user/navlist',
+               methods: 'post',
+               data: {...params}
+           }).then((res) => {
+            commit('setMenuTree', res)
+            dispatch("getFlatMenuList")
+            resolve(res)
+           })
+       })
+   },
+   getMenuList({state},params){
+        return new Promise((resolve) =>{
+            const { pid} = params
+            let permissionList = []
+            state.flatMenuList.forEach((v)=>{
+                if(v.pid == pid){
+                    permissionList.push(v)
+                }
+            })
+            resolve(permissionList)
+        })
+   },
+   // 将菜单列表扁平化形成权限列表
+   getFlatMenuList({commit,state}){
+        return new Promise((resolve) =>{
+            let permissionList = []
+            // 将菜单数据扁平化为一级
+            function flatNavList(arr){
+                for(let v of arr){
+                    permissionList.push(v)
+                    if(v.children && v.children.length){
+                        flatNavList(v.children)
+                    }
+                }
+            }
+            flatNavList(state.menuTree)
+            commit('setFlatMenuList', permissionList)
+            resolve()
+        })
     },
    // 获取该用户的菜单列表
    generateTopNavList({dispatch,state}){
